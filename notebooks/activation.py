16bb83eaa0b5b1957c6aa65e39bb10ed3f2de778
Efrat Hossain <efrathossain1130@gmail.com>
2026-08-20 22:45:23 -0400

    Add backprop

--- notebooks/activation.py
+++ notebooks/activation.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from abc import ABC, abstractmethod
 
@@ -9,13 +8,22 @@ class Activation(ABC):
 
   def __call__(self, inputs: np.ndarray):
     return self.forward(inputs)
+  
+  @abstractmethod
+  def backprop(self, gradient: np.ndarray) -> np.ndarray:
+    "Calculates the gradient of the activation function"
 
 class ReLU(Activation):
   def forward(self, inputs):
+    self.inp = inputs
     return np.maximum(0, inputs)
+  
+  def backprop(self, gradient):
+    return gradient * (self.inp > 0)
 
 class SoftMax(Activation):
   def forward(self, inputs):
+    self.inp = inputs
     expo = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
     norm = expo / np.sum(expo, axis=1, keepdims=True)
     return norm
--- notebooks/dense_layer.py
+++ notebooks/dense_layer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Optional
 
 class DenseLayer:
   def __init__(self, num_features: int, num_neurons: int):
@@ -17,10 +18,21 @@ class DenseLayer:
     return self._num_features
   
   def _random_weights(self) -> None:
-    self.weights = np.random.rand(self.num_neurons, self.num_features)
+    self.weights = np.random.rand(self.num_features, self.num_neurons)
   
   def _random_biases(self) -> None:
     self.biases = np.random.rand(1, self.num_neurons)
 
   def forward(self, samples: np.ndarray) -> np.ndarray:
-    return samples @ self.weights.T + self.biases
+    self.inp = samples
+    return samples @ self.weights + self.biases
+  
+  def backprop(self, gradient: Optional[np.ndarray] = None, stepsize: float = 0.001) -> np.ndarray:
+    if gradient is None:
+      return np.ones((len(self.inp), self.num_features))
+
+    outp = gradient @ self.weights.T
+    self.weights -= self.inp.T @ gradient * stepsize
+    self.biases -= np.sum(gradient, axis=0, keepdims=True) * stepsize
+    
+    return outp
